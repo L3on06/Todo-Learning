07fd1b4b9c1ebb656ff1a3f3e23653bf55178c4d
l3on06 <l3onshabani@gmail.com>
2024-10-31 14:28:53 +0100

    Save task in the local storage

--- store/task/index.ts
+++ store/task/index.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
 export type StatusProps = "TODO" | "IN_PROGRESS" | "DONE"
 
@@ -22,18 +23,23 @@ export type Action = {
 }
 
 
-export const useTaskStore = create<State & Action>()((set) => ({
-    tasks: [],
-    draggedTask: null,
-    createTask: (title: string, description?: string) => set((state) => ({ tasks: [...state.tasks, { id: Date.now().toString(), title, description, status: "TODO" }] })),
-    removeTask: (id: string) => set((state) => ({ tasks: state.tasks.filter((task) => task.id !== id) })),
-    updateTask: (id: string, status: StatusProps) => set((state) => ({
-        tasks: state.tasks.map((task) => {
-            if (task.id === id) {
-                return { ...task, status }
-            }
-            return task
-        })
-    })),
-    setDraggedTask: (id: string | null) => set({ draggedTask: id })
-}))
+export const useTaskStore = create<State & Action>()(
+    persist(
+        set => ({
+            tasks: [],
+            draggedTask: null,
+            createTask: (title: string, description?: string) => set((state) => ({ tasks: [...state.tasks, { id: Date.now().toString(), title, description, status: "TODO" }] })),
+            removeTask: (id: string) => set((state) => ({ tasks: state.tasks.filter((task) => task.id !== id) })),
+            updateTask: (id: string, status: StatusProps) => set((state) => ({
+                tasks: state.tasks.map((task) => {
+                    if (task.id === id) {
+                        return { ...task, status }
+                    }
+                    return task
+                })
+            })),
+            setDraggedTask: (id: string | null) => set({ draggedTask: id })
+        }),
+        { name: "task-storage" }
+    )
+)
