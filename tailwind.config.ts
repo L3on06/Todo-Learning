181ca17852a9ff747691559b39573de122ac51d6
l3on06 <l3onshabani@gmail.com>
2024-10-31 14:07:09 +0100

    Added the shadcn ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        md: '1.5rem',
-        lg: '2rem'
-      }
-    }
+  	container: {
+  		center: 'true',
+  		padding: {
+  			DEFAULT: '1rem',
+  			md: '1.5rem',
+  			lg: '2rem'
+  		}
+  	},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: []
+  plugins: [require("tailwindcss-animate")]
 }
